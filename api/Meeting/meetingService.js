ee8948244b38e4105bbaaad7a2202036babb45e9
Matthew Shortt <mattshortt4@gmail.com>
2019-06-28 13:35:52 -0400

    Add listTables

--- api/Meeting/meetingService.js
+++ api/Meeting/meetingService.js
@@ -1,18 +1,22 @@
 const AWS = require('aws-sdk');
 AWS.config.region = process.env.REGION;
 const ddb = new AWS.DynamoDB();
-const ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+const ddbTable = process.env.STARTUP_SIGNUP_TABLE;
 
 exports.dbTest = async () => {
     const params = {
-        ExclusiveStartTableName: ddbTable,
-        Limit: 10
+        'ExclusiveStartTableName': ddbTable,
+        'Limit': 10
     };
-    return ddb;
-    // await ddb.listTables(params, function(err, data) {
-    //     if (err) return err; // an error occurred
-    //     else     return data;
-    // });
+    // return ddb;
+    ddb.listTables(params, function (err, data) {
+        if (err) {
+            return err; // an error occurred
+        }
+        else {
+            return data;
+        }
+    });
 
     // return await {message: 'This test endpoint is within the api folder, within the controller and now the service!'};
 };
